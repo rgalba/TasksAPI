462a91c931fa7d2c4874b2c5b3caaf9039cde585
Rodrigo Galba Miranda <rodrigogalba@gmail.com>
2019-03-18 18:54:25 -0300

    add new feature

--- handler.rb
+++ handler.rb
@@ -5,7 +5,7 @@ def hello(event:, context:)
     puts "Received Request: #{event}"
 
     body = {
-      version: "V1.0.3",
+      version: "V1.0.4",
       payload: "Hello API."
     }
    { statusCode: 200, body: JSON.generate(body) }
